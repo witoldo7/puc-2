c9064fcd43813159d754e7a235ae34e64ef7b1db
witoldo7 <witusio@o2.pl>
2015-05-01 19:11:44 +0200

    takie se1

--- PUC/PUC_567/PUC/mod10.vhd
+++ PUC/PUC_567/PUC/mod10.vhd
@@ -4,11 +4,11 @@ use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
 entity mod10 is
-    Port (  outp : out std_logic_vector(3 downto 0);
-				dir : in std_logic;   
-				carry: out std_logic;   
-				clr : in std_logic;
-				clk : in std_logic);
+    Port(outp : out std_logic_vector(3 downto 0);
+	dir : in std_logic;   
+	carry: out std_logic;   
+	clr : in std_logic;
+	clk : in std_logic);
 end mod10;
 
 architecture Behavioral of mod10 is
@@ -26,9 +26,7 @@ begin
 				temp <= temp+1;
 				carry <='0';
 			else
-			if(dir = '1') then
 				temp <= "0000";
-				end if;
 				carry <= '1';
 			end if;		
 		elsif dir = '0' then
@@ -42,7 +40,5 @@ begin
 		end if;
 	end if;
 	outp<=temp;
-end process;  
-
-
+end process; 
 end Behavioral;
